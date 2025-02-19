3cab9f26fed5d805afbc1bdfd0de2f78dabb3189
Lalit Kumar <103807663+Lalitcodec@users.noreply.github.com>
2025-02-19 04:10:36 +0000

    completed

---
+++ 07ReactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+
+
+function Github() {
+    const [data,setData] = useState([])
+    useEffect(()=> {
+        fetch('https://api.github.com/users/hiteshchoudhary')
+        .then( response => response.json())
+        .then(data => {
+            console.log(data);
+            setData(data)
+        })
+
+    },[])
+
+    return (
+        <div className="text-center">
+            Github Followers : {data.followers}
+        </div>
+    )
+}
+
+export default Github
+
